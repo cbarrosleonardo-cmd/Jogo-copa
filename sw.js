c6f54895aedd97db4e10d3b674307048c54c55d3
cbarrosleonardo-cmd <cbarros.leonardo@gmail.com>
2026-09-23 23:37:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const C="leilao-copas-v2";
+const FILES=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"];
+self.addEventListener("install",e=>{ e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())); });
+self.addEventListener("activate",e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request).then(hit=>{
+    const net=fetch(e.request).then(r=>{ if(r && (r.ok||r.type==="opaque")){ const cp=r.clone(); caches.open(C).then(c=>c.put(e.request,cp)); } return r; }).catch(()=>hit);
+    return hit||net;
+  }));
+});
